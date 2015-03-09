790d0938456ffe4dfe19ba6625985d58bfb0c1e3
Darryl Walker <darryljwalker@gmail.com>
2015-03-09 20:33:25 +0000

    Fixing FOREIGN KEY error when removing FAQ question from FAQ group by making group_id nullable

---
+++ updates/group_id_is_nullable.php
@@ -0,0 +1,17 @@
+<?php namespace LaminSanneh\FantasticFaq\Updates;
+	
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class GroupIdIsNullable extends Migration
+{
+    public function up()
+    {
+        
+        Schema::table('laminsanneh_fantasticfaq_questions', function($table)
+		{
+		    $table->integer('group_id')->unsigned()->nullable()->change();
+		});
+        
+    }
+}
