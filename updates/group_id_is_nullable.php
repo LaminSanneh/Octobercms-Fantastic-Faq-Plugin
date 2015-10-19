a0b552baf57c46c868f1ea2f7254b80f7b516f9a
Trias Nur Rahman <contact@triasrahman.com>
2015-10-20 01:14:56 +0700

    Missed down() method

--- updates/group_id_is_nullable.php
+++ updates/group_id_is_nullable.php
@@ -1,5 +1,5 @@
 <?php namespace LaminSanneh\FantasticFaq\Updates;
-	
+
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
@@ -7,11 +7,19 @@ class GroupIdIsNullable extends Migration
 {
     public function up()
     {
-        
+
         Schema::table('laminsanneh_fantasticfaq_questions', function($table)
-		{
-		    $table->integer('group_id')->unsigned()->nullable()->change();
-		});
-        
+        {
+            $table->integer('group_id')->unsigned()->nullable()->change();
+        });
+
+    }
+
+    public function down()
+    {
+        Schema::table('laminsanneh_fantasticfaq_questions', function($table)
+        {
+            $table->integer('group_id')->unsigned()->change();
+        });
     }
 }
